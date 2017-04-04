faf605c904e27fe9bb7ecc00ca9d55d53545eef0
PhilippeWCS <philippedebridiers@gmail.com>
2017-04-04 12:18:04 +0200

    Install bootstrap with bower

---
+++ src/Wcs/PlatformBundle/Controller/Accueil.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder
+ * Date: 04/04/17
+ * Time: 10:05
+ */
+
+namespace Wcs\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class Accueil extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('WcsPlatformBundle:Default:index.html.twig');
+    }
+}
